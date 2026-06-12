3502636253274a87ca7b38431071fe78b246ef01
hontonoshin <146138410+hontonoshin@users.noreply.github.com>
2026-06-12 11:38:34 +0200

    Create ActionInitialization.hh

---
+++ include/ActionInitialization.hh
@@ -0,0 +1,9 @@
+#pragma once
+#include "G4VUserActionInitialization.hh"
+
+class ActionInitialization : public G4VUserActionInitialization
+{
+public:
+    void BuildForMaster() const override;
+    void Build() const override;
+};
