b211e5ec921799f19f8a4941660e670e3d24cd72
hontonoshin <146138410+hontonoshin@users.noreply.github.com>
2026-06-12 11:37:50 +0200

    Create ActionInitialization.cc

---
+++ src/ActionInitialization.cc
@@ -0,0 +1,16 @@
+#include "ActionInitialization.hh"
+#include "PrimaryGeneratorAction.hh"
+#include "EventAction.hh"
+#include "RunAction.hh"
+
+void ActionInitialization::BuildForMaster() const
+{
+    SetUserAction(new RunAction());
+}
+
+void ActionInitialization::Build() const
+{
+    SetUserAction(new PrimaryGeneratorAction());
+    SetUserAction(new RunAction());
+    SetUserAction(new EventAction());
+}
